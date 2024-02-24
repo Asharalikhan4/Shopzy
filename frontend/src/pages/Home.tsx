675b5ee93a1f13f59126aff49e7fd3ead5723e0c
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-24 22:05:26 +0530

    Cover page added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="home">
+            <section>
+
+            </section>
+            <h1>
+                Latest Products
+                <Link to={"/search"} className="findmore">More Items</Link>
+            </h1>
+            <main>
+
+            </main>
+        </div>
     );
 };
 
