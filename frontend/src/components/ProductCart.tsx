8144b804ddd06655c3c26c8db2f6ca40c9f47132
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-24 22:22:24 +0530

    Product card added

---
+++ frontend/src/components/ProductCart.tsx
@@ -0,0 +1,29 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductsProps = {
+    productId: string;
+    photo: string;
+    name: string;
+    price: number;
+    stock: number;
+    handler: () => void;
+}
+
+const server = "sjkfhksjd";
+
+const ProductCart = ({productId, price, name, photo, stock, handler}:ProductsProps) => {
+    return (
+        <div className="product-card">
+            <img src={photo} alt={name} />
+            <p>{name}</p>
+            <span>₹{price}</span>
+            <div>
+                <button onClick={() => handler()}>
+                    <FaPlus />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCart;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
+import ProductCart from "../components/ProductCart";
+
 const Home = () => {
+
+    const addToCartHandler = () => {
+
+    }
+
     return (
         <div className="home">
             <section>
@@ -11,7 +18,7 @@ const Home = () => {
                 <Link to={"/search"} className="findmore">More Items</Link>
             </h1>
             <main>
-
+                <ProductCart productId="jkahdk" name="MacBook" price={99999} stock={46} handler={addToCartHandler} photo="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1685966368/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256713_xqa1ds.png?tr=w-1000" />
             </main>
         </div>
     );
