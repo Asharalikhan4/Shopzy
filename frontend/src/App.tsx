3331ff8030e8fa47e5f7e6b3a4ed8ec926b15a22
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-25 00:25:23 +0530

    Login page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
 const Shipping = lazy(() => import("./pages/Shipping"));
+const Login = lazy(() => import("./pages/Login"));
 
 // Admin Routes Import
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -37,6 +38,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
           <Route>
             <Route path="/shipping" element={<Shipping />} />
           </Route>
